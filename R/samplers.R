31bff4a9ecc260ecb9b66508626e3e438a4dada6
AFEScalante <a.fer.esca@gmail.com>
2022-06-25 00:53:36 -0500

    Add normal objective function plus design

--- R/samplers.R
+++ R/samplers.R
@@ -1,3 +1,4 @@
+# Available objective functions
 log_exp_target <- function(x) dexp(x, rate = 1, log = TRUE)
 log_normal_target <- function(x) dnorm(x, mean = 0, sd = 1, log = TRUE)
 
@@ -6,7 +7,7 @@ metropolis_sampler <- function(log_target, n_iter, startval = 1, proposalsd = 1)
   x[1] <- startval
   for(i in 2:n_iter) {
     currentx <- x[i - 1]
-    proposedx <- rnorm(1, mean = currentx, sd = proposalsd) 
+    proposedx <- rnorm(1, mean = currentx, sd = proposalsd)
     A <- exp(log_target(proposedx) - log_target(currentx))
     if(runif(1) < A) {
       x[i] <- proposedx
--- app.R
+++ app.R
@@ -14,7 +14,7 @@ ui <- fluidPage(
         tags$link(href = "https://fonts.googleapis.com/css?family=Roboto+Mono", rel = "stylesheet"),
         includeCSS("www/styles.css")
     ),
-    
+
     # Sidebar layout
     sidebarLayout(
         sidebarPanel(
@@ -35,7 +35,7 @@ ui <- fluidPage(
             selectInput(
                 inputId = "method",
                 label = "Sampler Method",
-                choices = c("Metropolis", "Gibbs")
+                choices = c("Select method...", "Metropolis", "Gibbs")
             ),
             div(style="color: darkgray; width: 335px; align: center; margin-left: -5px;",
                 a(fa("github", fill = "darkgray", height = "2em"), href = "https://github.com/AFEScalante"),
@@ -50,21 +50,28 @@ ui <- fluidPage(
                              setSliderColor(c("#2a9d8f", "#2a9d8f"), c(1, 2)),
                              column(width = 3,
                                     selectInput(inputId = "metro_func", label = "Objective function",
-                                                choices = c("Exponential"))
+                                                choices = c("Exponential", "Normal"))
                              ),
                              column(width = 4,
                                     sliderInput(inputId = "metro_samples", label = "Number of samples",
-                                                min = 10, max = 1000, value = 100)
+                                                min = 10, max = 3000, value = 500)
                              ),
                              column(width = 4,
                                     sliderInput(inputId = "metro_step_sd", label = "SD proposal",
-                                                min = 0.1, max = 3, value = 1)
+                                                min = 0.1, max = 10, value = 1)
                              ),
                              fluidRow(
-                                 actionButton(inputId = "simulate", "Simulate"),
+                                 column(width = 1),
+                                 column(width = 8,
+                                        withMathJax(),
+                                        uiOutput('math_expr'),
+                                        actionButton(inputId = "simulate", "Simulate!")
+                                 )
+                             ),
+                             fluidRow(
                                  br(),
-                                 echarts4rOutput(outputId = "sims_metro_plot"),
-                                 echarts4rOutput(outputId = "sims_metro_hist")
+                                 echarts4rOutput(outputId = "sims_metro_hist", height = '250%'),
+                                 echarts4rOutput(outputId = "sims_metro_plot", height = '250%')
                              )
             ),
             conditionalPanel(condition = "input.method == 'Gibbs'",
@@ -77,12 +84,31 @@ ui <- fluidPage(
 
 # Server function
 server <- function(input, output) {
-    
+
+    # Math expressions
+    output$math_expr <- renderUI({
+        if (input$metro_func == "Exponential") {
+            withMathJax(helpText(paste0('Objective math expression: ',
+                                        '$$X \\sim \\Gamma(\\lambda = 1), \\quad ',
+                                        'f(x)=\\lambda e^{-\\lambda x}$$')))
+        } else if(input$metro_func == "Normal") {
+            withMathJax(helpText(paste0('Objective math expression:  ',
+                                       '$$X \\sim \\mathcal{N}(\\mu = 1, \\sigma = 0), \\quad ',
+                                       'f(x)=\\dfrac{1}{\\sigma \\sqrt{2\\pi}}e^{ -\\frac{1}{2} (\\dfrac{x - \\mu}{\\sigma})^{2}}$$')))
+        }
+    })
+
     data_sim <- eventReactive(input$simulate, {
+        metro_func <-
+            switch (input$metro_func,
+                    "Exponential" = log_exp_target,
+                    "Normal" = log_normal_target
+            )
+
         tibble(
             n_iter = 1:input$metro_samples,
             sims = metropolis_sampler(
-                log_target = log_exp_target,
+                log_target = metro_func,
                 n_iter = input$metro_samples,
                 startval = 1,
                 proposalsd = input$metro_step_sd
@@ -98,7 +124,7 @@ server <- function(input, output) {
             e_x_axis(n_iter) |>
             e_line(sims)
     })
-    
+
     output$sims_metro_hist <- renderEcharts4r({
         data_sim() |>
             e_charts() |>
@@ -111,8 +137,8 @@ server <- function(input, output) {
             e_color(c("#14213d", "#2a9d8f"), background = "#e5e5e5") |>
             e_tooltip(trigger = "axis")
     })
-    
+
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
