aed8fa5664ce8e2f6f777142cf0da00c3dede2cd
AFEScalante <a.fer.esca@gmail.com>
2022-06-24 23:17:52 -0500

    Add first files

---
+++ R/samplers.R
@@ -0,0 +1,18 @@
+log_exp_target <- function(x) dexp(x, rate = 1, log = TRUE)
+log_normal_target <- function(x) dnorm(x, mean = 0, sd = 1, log = TRUE)
+
+metropolis_sampler <- function(log_target, n_iter, startval = 1, proposalsd = 1) {
+  x <- numeric(n_iter)
+  x[1] <- startval
+  for(i in 2:n_iter) {
+    currentx <- x[i - 1]
+    proposedx <- rnorm(1, mean = currentx, sd = proposalsd) 
+    A <- exp(log_target(proposedx) - log_target(currentx))
+    if(runif(1) < A) {
+      x[i] <- proposedx
+    } else {
+      x[i] <- currentx
+    }
+  }
+  x
+}
---
+++ app.R
@@ -0,0 +1,118 @@
+library(shiny)
+library(shinyWidgets)
+library(tidyverse)
+library(fontawesome)
+library(echarts4r)
+library(here)
+
+# Source helper functions
+source(here("R", "samplers.R"))
+
+ui <- fluidPage(
+    # Head tags and styles
+    tags$head(
+        tags$link(href = "https://fonts.googleapis.com/css?family=Roboto+Mono", rel = "stylesheet"),
+        includeCSS("www/styles.css")
+    ),
+    
+    # Sidebar layout
+    sidebarLayout(
+        sidebarPanel(
+            id = "sidebar",
+            h1("MCMC Visualizer"),
+            br(),
+            p(paste0("Markov Chain Monte Carlo (MCMC) methods are primarily used for ",
+                     "calculating numerical approximations of multi-dimensional integrals, ",
+                     " for example in Bayesian statistics, computational physics, among others fields.",
+                     " Markov chain Monte Carlo methods create samples from a continuous random variable,",
+                     " with probability density proportional to a known function. These samples can be used to",
+                     " evaluate an integral over that variable, as its expected value or variance.",
+                     " Various algorithms exist for constructing chains, including:")),
+            HTML("<li>Metropolis-Hashting</li><li>Gibbs</li><li>Hamiltonian</li>"),
+            br(),
+            p(paste0("The purpose of this Shiny App is to visualize how different samplers works",
+                     " in an interactive way to help to grasp the idea behind each method.")),
+            selectInput(
+                inputId = "method",
+                label = "Sampler Method",
+                choices = c("Metropolis", "Gibbs")
+            ),
+            div(style="color: darkgray; width: 335px; align: center; margin-left: -5px;",
+                a(fa("github", fill = "darkgray", height = "2em"), href = "https://github.com/AFEScalante"),
+                a(fa("twitter", fill = "darkgray", height = "2em"), href = "https://twitter.com/angelfscal")
+            ),
+            width = 4
+        ),
+        mainPanel(
+            conditionalPanel(condition = "input.method == 'Metropolis'", offset = 4,
+                             br(),
+                             h2("Metropolis sampler"),
+                             setSliderColor(c("#2a9d8f", "#2a9d8f"), c(1, 2)),
+                             column(width = 3,
+                                    selectInput(inputId = "metro_func", label = "Objective function",
+                                                choices = c("Exponential"))
+                             ),
+                             column(width = 4,
+                                    sliderInput(inputId = "metro_samples", label = "Number of samples",
+                                                min = 10, max = 1000, value = 100)
+                             ),
+                             column(width = 4,
+                                    sliderInput(inputId = "metro_step_sd", label = "SD proposal",
+                                                min = 0.1, max = 3, value = 1)
+                             ),
+                             fluidRow(
+                                 actionButton(inputId = "simulate", "Simulate"),
+                                 br(),
+                                 echarts4rOutput(outputId = "sims_metro_plot"),
+                                 echarts4rOutput(outputId = "sims_metro_hist")
+                             )
+            ),
+            conditionalPanel(condition = "input.method == 'Gibbs'",
+                             br(),
+                             h2("Gibbs Sampler")),
+            width = 8
+        )
+    )
+)
+
+# Server function
+server <- function(input, output) {
+    
+    data_sim <- eventReactive(input$simulate, {
+        tibble(
+            n_iter = 1:input$metro_samples,
+            sims = metropolis_sampler(
+                log_target = log_exp_target,
+                n_iter = input$metro_samples,
+                startval = 1,
+                proposalsd = input$metro_step_sd
+            )
+        )
+    })
+
+    output$sims_metro_plot <- renderEcharts4r({
+        data_sim() |>
+            e_charts(x = n_iter) |>
+            e_color("#14213d", background = "#e5e5e5") |>
+            e_tooltip() |>
+            e_x_axis(n_iter) |>
+            e_line(sims)
+    })
+    
+    output$sims_metro_hist <- renderEcharts4r({
+        data_sim() |>
+            e_charts() |>
+            e_histogram(sims, name = "histogram") |>
+            e_density(sims,
+                      areaStyle = list(opacity = .4),
+                      smooth = TRUE,
+                      name = "density",
+                      y_index = 1) |>
+            e_color(c("#14213d", "#2a9d8f"), background = "#e5e5e5") |>
+            e_tooltip(trigger = "axis")
+    })
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
